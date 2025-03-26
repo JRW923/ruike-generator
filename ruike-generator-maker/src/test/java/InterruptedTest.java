08fe9fa3d9bbc73c371dbcbc5a4c52aac8c52a1f
“wjr” <wjr@itcast.cn>
2025-03-26 20:57:15 +0800

    云平台开发-开发在线代码生成器平台的管理页面和主页搜索列表

---
+++ ruike-generator-maker/src/test/java/InterruptedTest.java
@@ -0,0 +1,8 @@
+
+
+
+public class InterruptedTest {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
---
+++ ruike-generator-maker/src/test/java/Solution.java
@@ -0,0 +1,11 @@
+/**
+ * @author: WJR
+ * @create: 2025-02-02 12:17
+ * @description:
+ */
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
+
